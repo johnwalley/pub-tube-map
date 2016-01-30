8eac09a4616e0ed98a2fb8a741c7f76cc1019d89
John.Walley <john@walley.org.uk>
2016-01-30 22:16:47 +0000

    Assortment of random changes

--- src/Lines.js
+++ src/Lines.js
@@ -1,3 +1,19 @@
 var Lines = function (lines) {
     this.lines = lines;
 };
+
+Lines.prototype.highlightLine = function(name) {
+  this.lines.forEach(function(line) {
+     if (line.name === name) {
+         line.highlighted = true;
+     }
+  });
+};
+
+Lines.prototype.unhighlightLine = function(name) {
+    this.lines.forEach(function(line) {
+        if (line.name === name) {
+            line.highlighted = false;
+        }
+    });
+};
